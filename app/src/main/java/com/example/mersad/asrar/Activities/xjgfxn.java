1dd7fbfff55c13fa26e9ac6b509b36ffa5fc6cea
hamed.haghighiasl1991 <hamed.haghighiasl1991@outlook.com>
2019-01-03 11:56:19 +0330

    کلاسه جدید

--- app/src/main/java/com/example/mersad/asrar/Activities/xjgfxn.java
+++ app/src/main/java/com/example/mersad/asrar/Activities/xjgfxn.java
@@ -1,7 +1,24 @@
 package com.example.mersad.asrar.Activities;
 
+import android.graphics.Typeface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
 public class xjgfxn {
 
-    hi ;
+    Button Rating_Activity_Btn_Save_Rate;
+    private RatingBar Rating_Activity_Rb_Progress, Rating_Activity_Rb_Knowledge, Rating_Activity_Rb_Clean, Rating_Activity_Rb_Light_And_Air, Rating_Activity_Rb_Facilities;
+    private TextView Rating_Activity_Tv_Progress, Rating_Activity_Tv_Knowledge, Rating_Activity_Tv_Clean, Rating_Activity_Tv_Light_And_Air, Rating_Activity_Tv_Facilities;
+    Typeface custom_font;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
 
 }
